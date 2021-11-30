d163d90273b19c6c59f911fddfd5258465fb57db
unknown <kamrunnesasamiya1997@gmail.com>
2021-11-30 12:52:15 +0600

    new java file

---
+++ lab.java
@@ -0,0 +1,11 @@
+public class Lab {
+    private int var, varn;
+ 
+    public int getter() {
+        return this.var;
+    }
+ 
+    public void setter(int num) {
+        this.var = var;
+    }
+}
